dd7265c5d08c34ac9b510c103c1a38e556c8d8b8
shanajasila <shanajasilam@gmail.com>
2022-11-21 11:07:14 +0530

    fixed

--- src/Menudriven.java
+++ src/Menudriven.java
@@ -2,28 +2,29 @@ import java.util.Scanner;
 
 public class Menudriven {
     public static void main(String args[]){
-        System.out.println("enter two numbers");
-        Scanner sc=new Scanner(System.in);
-        float a= sc.nextFloat();
-        float b= sc.nextFloat();
-        System.out.println("please Select your option");
         System.out.println("menu:\n 1.Addition\n 2.Substraction\n 3.Multiplication\n4.Division");
+        System.out.println("\nplease Select your option");
+        Scanner sc=new Scanner(System.in);
         int menu= sc.nextInt();
+        System.out.println("\nenter two numbers");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+
         switch(menu){
             case 1:
-                float c=a+b;
+                int c=a+b;
                 System.out.println(a+" + "+b+"="+c);
                 break;
             case 2:
-                float s=a-b;
+                int s=a-b;
                 System.out.println(a+" - "+b+"="+s);
                 break;
             case 3:
-                float m=a*b;
+                int m=a*b;
                 System.out.println(a+" * "+b+"="+m);
                 break;
             case 4:
-                float d=a/b;
+                int d=a/b;
                 System.out.println(a+" / "+b+"="+d);
                 break;
             default:
