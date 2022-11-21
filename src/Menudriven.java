6528ccdd1ca7f8ce3a6fac4a59c6e5007a8427ec
shanajasila <shanajasilam@gmail.com>
2022-11-21 10:33:43 +0530

    created

--- src/Menudriven.java
+++ src/Menudriven.java
@@ -1,3 +1,15 @@
-public class Menudriven {
+import java.util.Scanner;
 
+public class Menudriven {
+    public static void main(String args[]){
+        System.out.println("enter two numbers");
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println("please Select your option");
+        System.out.println("menu: 1.Addition\n 2.Substraction\n 3.Multiplication\n4.Division");
+        int menu= sc.nextInt();
+
+
+    }
 }
