824fd1ee430ddb7e8f6fc51be280d5e0a930d0cb
shanajasila <shanajasilam@gmail.com>
2022-11-21 12:07:11 +0530

    completed

--- src/Menudriven.java
+++ src/Menudriven.java
@@ -1,34 +1,47 @@
 import java.util.Scanner;
 
 public class Menudriven {
-    public static void main(String args[]){
-        System.out.println("enter two numbers");
-        Scanner sc=new Scanner(System.in);
-        float a= sc.nextFloat();
-        float b= sc.nextFloat();
-        System.out.println("please Select your option");
-        System.out.println("menu:\n 1.Addition\n 2.Substraction\n 3.Multiplication\n4.Division");
-        int menu= sc.nextInt();
-        switch(menu){
-            case 1:
-                float c=a+b;
-                System.out.println(a+" + "+b+"="+c);
-                break;
-            case 2:
-                float s=a-b;
-                System.out.println(a+" - "+b+"="+s);
-                break;
-            case 3:
-                float m=a*b;
-                System.out.println(a+" * "+b+"="+m);
-                break;
-            case 4:
-                float d=a/b;
-                System.out.println(a+" / "+b+"="+d);
-                break;
-            default:
-                System.out.println("please select from given menu");
-        }
+    public static void main(String args[]) {
 
-    }
+            System.out.println("please Select your option");
+            System.out.println("menu:\n 1.Addition\n 2.Substraction\n 3.Multiplication\n4.Division");
+            Scanner sc = new Scanner(System.in);
+            int menu = sc.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("enter two numbers");
+                    int a = sc.nextInt();
+                    int b = sc.nextInt();
+                    int c = a + b;
+                    System.out.println(a + " + " + b + "=" + c);
+                    break;
+                case 2:
+                    System.out.println("enter two numbers");
+                    int a1 = sc.nextInt();
+                    int b1 = sc.nextInt();
+                    int s = a1 - b1;
+                    System.out.println(a1 + " - " + b1 + "=" + s);
+                    break;
+                case 3:
+                    System.out.println("enter two numbers");
+                    int a2 = sc.nextInt();
+                    int b2 = sc.nextInt();
+                    int m = a2 * b2;
+                    System.out.println(a2 + " * " + b2 + "=" + m);
+                    break;
+                case 4:
+                    System.out.println("enter two numbers");
+                    int a3 = sc.nextInt();
+                    int b3 = sc.nextInt();
+                    int d = a3 / b3;
+                    System.out.println(a3 + " / " + b3 + "=" + d);
+                    break;
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println("please select from given menu");
+            }
+
+        }
 }
